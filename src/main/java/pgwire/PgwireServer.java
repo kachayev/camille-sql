ec1151799619a7f0aa04759f4eb8fe88c48bcaeb
Oleksii Kachaiev <kachayev@gmail.com>
2020-05-16 02:28:35 -0700

    Properly parse arguments

--- src/main/java/pgwire/PgwireServer.java
+++ src/main/java/pgwire/PgwireServer.java
@@ -1,5 +1,13 @@
 package pgwire;
 
+import java.io.File;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -8,15 +16,48 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import m2sql.MavenArtifactsDatabase;
 
-public class PgwireServer 
-{
+public class PgwireServer {
     final static int DEFAULT_PORT = 26727;
 
     final static String DEFAUL_FOLDER = System.getProperty("user.home") + "/.m2/repository/";
 
-    public static void main(String[] args) throws InterruptedException
-    {
-        final MavenArtifactsDatabase db = new MavenArtifactsDatabase(DEFAUL_FOLDER);
+    final static CommandLineParser parser = new DefaultParser();
+
+    final static Options parserOptions = new Options();
+
+    static {
+        parserOptions.addOption("p", "port", true, "port");
+        parserOptions.addOption("r", "repository", true,
+            "path to maven artifacts repository (defaults to ~/.m2/repository)");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(parserOptions, args);
+        } catch (ParseException e) {
+            System.out.print("Arguments error: ");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        int port = DEFAULT_PORT;
+        if (cmd.hasOption("p")) {
+            port = Integer.valueOf(cmd.getOptionValue("p"));
+        }
+
+        String folder = DEFAUL_FOLDER;
+        if (cmd.hasOption("r")) {
+            folder = cmd.getOptionValue("r").strip();
+            final File f = new File(folder);
+            if (!f.exists() || !f.isDirectory()) {
+                System.out.println(String.format("Invalid reposiroty directory: %s", folder));
+                System.exit(1);
+            }
+            folder = f.getAbsolutePath();
+        }
+
+        final MavenArtifactsDatabase db = new MavenArtifactsDatabase(folder);
 
         EventLoopGroup boss = new NioEventLoopGroup(1);
         EventLoopGroup worker = new NioEventLoopGroup();
@@ -26,7 +67,10 @@ public class PgwireServer
                 .channel(NioServerSocketChannel.class)
                 .handler(new LoggingHandler(LogLevel.INFO))
                 .childHandler(new PgwireServerInitializer(db));
-            b.bind(DEFAULT_PORT).sync().channel().closeFuture().sync();
+            
+            System.out.println(String.format("Running server on localhost:%d", port));
+
+            b.bind(port).sync().channel().closeFuture().sync();
         } finally {
             boss.shutdownGracefully();
             worker.shutdownGracefully();
