a16a8a10850dd77cab7d96f5de5801237388da52
Oleksii Kachaiev <kachayev@gmail.com>
2020-05-28 22:38:45 -0700

    pretty printer for logical plan

--- src/main/java/m2sql/MavenArtifactsQueryPlanner.java
+++ src/main/java/m2sql/MavenArtifactsQueryPlanner.java
@@ -1,5 +1,8 @@
 package m2sql;
 
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +14,7 @@ import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.plan.RelTraitDef;
 import org.apache.calcite.rel.RelCollationTraitDef;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
 import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.sql.SqlDialect;
@@ -19,6 +23,7 @@ import org.apache.calcite.sql.dialect.AnsiSqlDialect;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.calcite.sql.parser.babel.SqlBabelParserImpl;
+import org.apache.calcite.sql.util.SqlString;
 import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.Planner;
@@ -30,11 +35,16 @@ public class MavenArtifactsQueryPlanner {
 
     private final Planner planner;
 
+    private final AnsiSqlDialect sqlDialect;
+
     public MavenArtifactsQueryPlanner(SchemaPlus schema) {
         final List<RelTraitDef> traitDefs = new ArrayList<>();
 
         traitDefs.add(ConventionTraitDef.INSTANCE);
         traitDefs.add(RelCollationTraitDef.INSTANCE);
+
+        sqlDialect = new AnsiSqlDialect(SqlDialect.EMPTY_CONTEXT.withDatabaseProduct(SqlDialect.DatabaseProduct.UNKNOWN)
+                .withIdentifierQuoteString("'"));
 
         FrameworkConfig frameworkConfig = Frameworks.newConfigBuilder()
                 .parserConfig(SqlParser.configBuilder().setLex(Lex.MYSQL_ANSI).build()).defaultSchema(schema)
@@ -44,39 +54,36 @@ public class MavenArtifactsQueryPlanner {
         this.planner = Frameworks.getPlanner(frameworkConfig);
     }
 
-    public RelNode getLogicalPlan(String query) throws ValidationException, RelConversionException {
-        SqlNode sqlNode;
-
-        try {
-            sqlNode = planner.parse(query);
-        } catch (SqlParseException e) {
-            throw new RuntimeException("Query parsing error.", e);
-        }
-
-        SqlNode validateSqlNode = planner.validate(sqlNode);
-        return planner.rel(validateSqlNode).project();
+    public RelRoot getLogicalPlan(String query) throws ValidationException, RelConversionException, SqlParseException {
+        final SqlNode sqlNode = this.planner.parse(query);
+        final SqlNode validateSqlNode = this.planner.validate(sqlNode);
+        return planner.rel(validateSqlNode);
     }
 
-    public static void main(String[] args) {
-        final SqlParser.Config parserConfig = SqlParser.configBuilder()
-            .setLex(Lex.MYSQL_ANSI)
-            .setParserFactory(SqlBabelParserImpl.FACTORY)
-            .build();
+    public SqlString parse(String query) throws SqlParseException {
+        final SqlNode node = this.planner.parse(query);
+        return node.toSqlString(this.sqlDialect);
+    }
 
-        final FrameworkConfig frameworkConfig = Frameworks.newConfigBuilder()
-                .parserConfig(parserConfig)
-                .context(Contexts.EMPTY_CONTEXT)
-                .ruleSets(RuleSets.ofList())
-                .costFactory(null)
-                .typeSystem(RelDataTypeSystem.DEFAULT)
-                .build();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        final Planner planner = Frameworks.getPlanner(frameworkConfig);
+        final String userFolder = System.getProperty("user.home");
+        final String m2Folder = Paths.get(userFolder, ".m2/repository/").toString();
+        MavenArtifactsDatabase db = new MavenArtifactsDatabase(m2Folder);
 
-        try {
-            final SqlNode node = planner.parse("SELECT pg_catalog.array_to_string(d.datacl, '\"\n\"') AS \"Access privileges\" FROM pg_catalog.pg_database d ORDER BY 1");
-            System.out.println(node.toSqlString(AnsiSqlDialect.DEFAULT));
-        } catch (SqlParseException e) {
+        try (final Connection conn = db.createConnection()) {
+            final CalciteConnection calciteConnection = (CalciteConnection) conn;
+            final MavenArtifactsQueryPlanner planner = new MavenArtifactsQueryPlanner(calciteConnection.getRootSchema());
+
+            // final SqlString node = planner.parse("select group_id, count(*) as n_files from versions group by group_id order by n_files desc");
+            // System.out.println(node);
+
+            // final RelRoot root = planner.getLogicalPlan("select group_id, name from artifacts");
+            // final RelRoot root = planner.getLogicalPlan("select group_id, name from artifacts where group_id = 'nrepl' order by artifact_id limit 20");
+            final RelRoot root = planner.getLogicalPlan("select group_id, count(*) as n_files from artifacts left join versions on artifacts.uid=versions.uid group by group_id order by n_files desc");
+            System.out.println(RelOptUtil.toString(root.project()));
+            System.out.println(root);
+        } catch (SqlParseException | RelConversionException | ValidationException e) {
             e.printStackTrace();
         }
     }
